6ec2eda645378d60ee63aafb1a38a9cae2a90735
RicardoBortolin <99661571+RicardoBortolin@users.noreply.github.com>
2022-02-15 23:28:26 -0300

    Add files via upload
    Exercises from beecrowd

---
+++ SimpleProduct.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    double A, B, PROD;
+        scanf("%lf %lf ", &A, &B);
+    
+    PROD = A * B;
+
+    printf("PROD = %.1lf\n", PROD); 
+        return 0;
+    }
+
+
